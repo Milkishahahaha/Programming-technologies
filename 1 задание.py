1cb28bde73eb1649f83a1cb01068be2019536a5d
Milkishahahaha <milafashutdinova@gmail.com>
2025-12-04 00:41:03 +0300

    Add files via upload

---
+++ 1 задание.py
@@ -0,0 +1,31 @@
+class Student:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.marks = [] 
+    
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+    
+    def add_mark(self, mark):
+        if 1 <= mark <= 5:
+            self.marks.append(mark)
+        else:
+            print(f"Оценка {mark} недопустима. Допустимы оценки от 1 до 5.")
+    
+    def get_average_mark(self):
+        if not self.marks:  
+            return 0.0
+        return sum(self.marks) / len(self.marks)
+    
+#Пример использования 
+student = Student("Иван", "Петров")
+print(student.get_full_name()) 
+
+student.add_mark(5)
+student.add_mark(4)
+student.add_mark(3)
+student.add_mark(6)  
+
+print(student.get_average_mark())
+    
